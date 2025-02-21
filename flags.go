5d8fb0a68f9114fc98bc39c203df47a59851b9b7
Jamie Thomas <jamiewthomas13@gmail.com>
2025-02-21 14:05:09 +0000

    Fix typo

--- flags.go
+++ flags.go
@@ -13,7 +13,7 @@ func ParseFlags() Config {
 
 	flag.StringVar(&config.Add, "add", "", "Add a new todo item")
 	flag.IntVar(&config.Remove, "remove", -1, "Remove the todo item at given index")
-	flag.IntVar(&config.Remove, "toggle", -1, "Toggle todo item completion state at given index")
+	flag.IntVar(&config.Toggle, "toggle", -1, "Toggle todo item completion state at given index")
 
 	flag.Parse()
 
