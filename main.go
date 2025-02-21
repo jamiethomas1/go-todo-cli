33d4fdac476fd9e1a4586051d5f9ec7d08837542
Jamie Thomas <jamiewthomas13@gmail.com>
2025-02-21 14:05:50 +0000

    Add --toggle flag

--- main.go
+++ main.go
@@ -32,17 +32,40 @@ func main() {
 
 		tl.push(ti)
 	} else if config.Remove != -1 {
-		item, err := tl.get(config.Remove - 1)
+		item, err := tl.getTask(config.Remove - 1)
 		if err != nil {
 			log.Fatal("error getting todo item at given index")
 		}
-
-		log.Printf("Removing item \"%s\"\n", item)
 
 		err = tl.drop(config.Remove - 1)
 		if err != nil {
 			log.Fatal("error removing todo item at given index")
 		}
+
+		log.Printf("Removed item \"%s\"\n", item)
+	} else if config.Toggle != -1 {
+		item, err := tl.getTask(config.Toggle - 1)
+		if err != nil {
+			log.Fatal("error getting todo item at given index")
+		}
+
+		err = tl.toggleItem(config.Toggle - 1)
+		if err != nil {
+			log.Fatal("error toggling item")
+		}
+
+		var status string
+		s, err := tl.getComplete(config.Toggle - 1)
+		if err != nil {
+			log.Fatal("error getting completion status of task at given index")
+		}
+		if s {
+			status = "Complete"
+		} else {
+			status = "Not Complete"
+		}
+
+		log.Printf("Toggled item \"%s\" to \"%s\"\n", item, status)
 	}
 
 	err = writeTodoList(&tl)
--- todolist.go
+++ todolist.go
@@ -7,6 +7,10 @@ type TodoItem struct {
 	Complete bool   `json:"complete"`
 }
 
+func (ti *TodoItem) toggleComplete() {
+	ti.Complete = !ti.Complete
+}
+
 type TodoList struct {
 	Items []TodoItem `json:"items"`
 }
@@ -15,12 +19,30 @@ func (tl *TodoList) push(ti TodoItem) {
 	tl.Items = append(tl.Items, ti)
 }
 
-func (tl *TodoList) get(index int) (string, error) {
+func (tl *TodoList) getTask(index int) (string, error) {
 	if index < 0 || index >= len(tl.Items) {
 		return "", fmt.Errorf("index out of range")
 	}
 
 	return tl.Items[index].Task, nil
+}
+
+func (tl *TodoList) getComplete(index int) (bool, error) {
+	if index < 0 || index >= len(tl.Items) {
+		return false, fmt.Errorf("index out of range")
+	}
+
+	return tl.Items[index].Complete, nil
+}
+
+func (tl *TodoList) toggleItem(index int) error {
+	if index < 0 || index >= len(tl.Items) {
+		return fmt.Errorf("index out of range")
+	}
+
+	tl.Items[index].Complete = !tl.Items[index].Complete
+
+	return nil
 }
 
 func (tl *TodoList) drop(index int) error {
